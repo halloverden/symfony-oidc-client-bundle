290f8c40e7854ba987a2144b9bec2df1c127a212
Tom Roar Furunes <tom.roar@halloverden.no>
2020-07-30 19:18:26 +0200

    ADD: HandleAuthCodeRequest

---
+++ Entity/Requests/HandleAuthCodeRequest.php
@@ -0,0 +1,57 @@
+<?php
+
+
+namespace HalloVerden\Oidc\ClientBundle\Entity\Requests;
+
+/**
+ * Class HandleAuthCodeRequest
+ *
+ * @package HalloVerden\Oidc\ClientBundle\Entity\Requests
+ */
+class HandleAuthCodeRequest {
+
+  /**
+   * @var string
+   */
+  private $code;
+
+  /**
+   * @var string|null
+   */
+  private $state;
+
+  /**
+   * @return string
+   */
+  public function getCode(): string {
+    return $this->code;
+  }
+
+  /**
+   * @param string $code
+   *
+   * @return HandleAuthCodeRequest
+   */
+  public function setCode(string $code): self {
+    $this->code = $code;
+    return $this;
+  }
+
+  /**
+   * @return string|null
+   */
+  public function getState(): ?string {
+    return $this->state;
+  }
+
+  /**
+   * @param string|null $state
+   *
+   * @return HandleAuthCodeRequest
+   */
+  public function setState(?string $state): self {
+    $this->state = $state;
+    return $this;
+  }
+
+}
