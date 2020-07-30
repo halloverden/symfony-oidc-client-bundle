b5994548e6944aab6bd151d447787fe01935b973
Tom Roar Furunes <tom.roar@halloverden.no>
2020-07-30 18:37:24 +0200

    ADD: grants

---
+++ Entity/Grant/RefreshTokenGrant.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace HalloVerden\Oidc\ClientBundle\Entity\Grant;
+
+
+use HalloVerden\Oidc\ClientBundle\Interfaces\Grant\OidcGrantInterface;
+
+class RefreshTokenGrant implements OidcGrantInterface {
+  const TYPE_NAME = 'refresh_token';
+
+  /**
+   * @var string
+   */
+  private $refreshToken;
+
+  /**
+   * RefreshTokenGrant constructor.
+   *
+   * @param string $refreshToken
+   */
+  public function __construct(string $refreshToken) {
+    $this->refreshToken = $refreshToken;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function getRequestData(): array {
+    return [
+      'refresh_token'
+    ];
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function getTypeName(): string {
+    return self::TYPE_NAME;
+  }
+
+}
