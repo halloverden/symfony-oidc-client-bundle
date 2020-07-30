3b6a0699ec7734df645103636e633eeb0bf6ee53
Tom Roar Furunes <tom.roar@halloverden.no>
2020-07-30 18:37:40 +0200

    ADD: RandomHelper

---
+++ Entity/Helpers/RandomHelper.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace HalloVerden\Oidc\ClientBundle\Entity\Helpers;
+
+
+class RandomHelper {
+
+  /**
+   * @param int $length
+   * @param bool $urlSafe
+   * @param bool $includeNumbers
+   * @return string
+   */
+  public static function generateRandomString($length = 10, $urlSafe = false, $includeNumbers = true) {
+    $UrlUnsafecharacters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ/-+*%&¤#\"\\=(){}';
+    $UrlSafecharacters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    $characters = $urlSafe ? $UrlSafecharacters : $UrlUnsafecharacters;
+
+    if ($includeNumbers) {
+      $characters .= '1234567890';
+    }
+
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+      $randomString .= $characters[rand(0, strlen($characters) - 1)];
+    }
+    return $randomString;
+  }
+
+}
