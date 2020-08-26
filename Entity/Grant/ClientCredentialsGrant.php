ba2c0e6b9adb2f12ee0f6a5b42078eb3d4eb5203
tomme87 <tom.roar@halloverden.no>
2020-08-26 15:35:12 +0200

    ADD: ability to override scope param for client credentials grant.

--- Entity/Grant/ClientCredentialsGrant.php
+++ Entity/Grant/ClientCredentialsGrant.php
@@ -6,11 +6,39 @@ namespace HalloVerden\Oidc\ClientBundle\Entity\Grant;
 
 use HalloVerden\Oidc\ClientBundle\Interfaces\Grant\OidcGrantInterface;
 
+/**
+ * Class ClientCredentialsGrant
+ *
+ * @package HalloVerden\Oidc\ClientBundle\Entity\Grant
+ */
 class ClientCredentialsGrant implements OidcGrantInterface {
   const TYPE_NAME = 'client_credentials';
 
+  /**
+   * @var array|null
+   */
+  private $scopes;
+
+  /**
+   * ClientCredentialsGrant constructor.
+   *
+   * @param array|null $scopes
+   */
+  public function __construct(?array $scopes = null) {
+    $this->scopes = $scopes;
+  }
+
+  /**
+   * @inheritDoc
+   */
   public function getRequestData(): array {
-    return [];
+    $data = [];
+
+    if (null !== $scope = $this->getScope()) {
+      $data['scope'] = $scope;
+    }
+
+    return $data;
   }
 
   /**
@@ -18,6 +46,17 @@ class ClientCredentialsGrant implements OidcGrantInterface {
    */
   public function getTypeName(): string {
     return self::TYPE_NAME;
+  }
+
+  /**
+   * @return string|null
+   */
+  private function getScope(): ?string {
+    if (null === $this->scopes) {
+      return null;
+    }
+
+    return implode(' ', $this->scopes);
   }
 
 }
