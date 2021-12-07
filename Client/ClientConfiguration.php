be6a01d834927aabdaf828eab86a327f5d54c275
tomme87 <tom.roar@halloverden.no>
2021-12-07 12:38:01 +0100

    ADD: stateParameterLength + nonceParameterLength

--- Client/ClientConfiguration.php
+++ Client/ClientConfiguration.php
@@ -15,6 +15,8 @@ class ClientConfiguration {
   private ?string $acrValues = null;
   private string $scope;
   private bool $pkceEnabled = false;
+  private int $stateParameterLength = 10;
+  private int $nonceParameterLength = 10;
 
   /**
    * ClientConfiguration constructor.
@@ -202,6 +204,34 @@ class ClientConfiguration {
   public function setPkceEnabled(bool $pkceEnabled): self {
     $this->pkceEnabled = $pkceEnabled;
     return $this;
+  }
+
+  /**
+   * @return int
+   */
+  public function getStateParameterLength(): int {
+    return $this->stateParameterLength;
+  }
+
+  /**
+   * @param int $stateParameterLength
+   */
+  public function setStateParameterLength(int $stateParameterLength): void {
+    $this->stateParameterLength = $stateParameterLength;
+  }
+
+  /**
+   * @return int
+   */
+  public function getNonceParameterLength(): int {
+    return $this->nonceParameterLength;
+  }
+
+  /**
+   * @param int $nonceParameterLength
+   */
+  public function setNonceParameterLength(int $nonceParameterLength): void {
+    $this->nonceParameterLength = $nonceParameterLength;
   }
 
 }
